cf4def4f88b84572e2daace6a505ca90776c3bcb
Jakub Czarnecki <jakubczarnecki@poczta.onet.pl>
2025-01-22 19:22:45 +0100

    Initial commit

---
+++ src/main/java/com/example/JavaZadanieJakubCzarnecki/JavaZadanieJakubCzarneckiApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaZadanieJakubCzarnecki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaZadanieJakubCzarneckiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaZadanieJakubCzarneckiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaZadanieJakubCzarnecki/JavaZadanieJakubCzarneckiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaZadanieJakubCzarnecki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaZadanieJakubCzarneckiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
